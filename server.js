1cad9eb91e2ab3a3719f8aa32aaa2cfd20ab286b
Bhavesh-26sareen <sareenbhavesh26@gmail.com>
2020-12-17 18:11:13 +0530

    homepage done

--- server.js
+++ server.js
@@ -3,18 +3,20 @@ const app = express()
 
 const ejs = require('ejs')
 const path = require('path')
+
 const expressLayout = require('express-Ejs-layouts')
 
-const PORT = process.env.PORT || 3000
-
+const PORT = process.env.PORT || 3200
+app.use(express.static('public'))
 app.get('/' , (req,res) => {
-    res.render('home')
+    res.render('home') // here you render `home` template
 })
 
 // set template engine
 app.use(expressLayout)
-app.set('views', path.join(__dirname,'/resources/views'))
-app.set('view engine','ejs')
+app.set('views', path.join(__dirname,'/resources/views')) // here you set that all templates are located in `/views` directory
+app.set('view engine','ejs')//here you set that you're using `ejs` template engine, and the
+                            // default extension is `ejs`
 
 
 app.listen(PORT , () => {
