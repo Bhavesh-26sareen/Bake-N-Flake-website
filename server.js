7e895c124ec6a22d0800385a57ad43d56e297b6d
Bhavesh-26sareen <sareenbhavesh26@gmail.com>
2020-12-18 13:46:30 +0530

    auth and cart page done

--- server.js
+++ server.js
@@ -1,22 +1,37 @@
 const express = require('express')
 const app = express() 
-
+  
 const ejs = require('ejs')
 const path = require('path')
 
 const expressLayout = require('express-Ejs-layouts')
 
-const PORT = process.env.PORT || 3200
+const PORT = process.env.PORT || 3300
 app.use(express.static('public'))
+
+
+
+// set template engine 
+app.use(expressLayout) //code which will be used again and again will be put in layout.ejs
+app.set('views', path.join(__dirname,'/resources/views')) // here you set that all templates are located in `/views` directory
+app.set('view engine','ejs')//here you set that you're using `ejs` template engine, and the
+                            // default extension is `ejs`
+ 
 app.get('/' , (req,res) => {
     res.render('home') // here you render `home` template
 })
 
-// set template engine
-app.use(expressLayout)
-app.set('views', path.join(__dirname,'/resources/views')) // here you set that all templates are located in `/views` directory
-app.set('view engine','ejs')//here you set that you're using `ejs` template engine, and the
-                            // default extension is `ejs`
+
+app.get('/cart' , (req,res) => {
+  res.render('customers/cart') //render cart.ejs template
+})
+
+app.get('/login' , (req,res) => {
+  res.render('auth/login')
+})
+app.get('/register' , (req,res) => {
+  res.render('auth/register')
+})
 
 
 app.listen(PORT , () => {
